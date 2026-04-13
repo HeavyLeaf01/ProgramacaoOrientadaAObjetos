9aef60cc44a2a50cab649bd9e4d97ddd1e781d48
HeavyLeaf <gabigoolgabriell@gmail.com>
2026-04-13 08:58:59 -0300

    Update

--- Atividade13.py
+++ Atividade13.py
@@ -1,18 +1,22 @@
+from datetime import date
 class Fornecedor:
     def __init__(self, CNPJ: str,nome: str):
         self.CNPJ = CNPJ
         self.nome = nome
+        self.listaVendaProdutos = []
 
+    def addVenda(self, q, dv, p):
+        self.listaVendaProdutos.append(Venda(self,q,dv,p))
     def emitirNota(self, di: date, df: date) -> float:
-        self.di = di
-        self.df = df
+        pass
+
 class Produto:
     def __init__(self, codigo: int, descricao: str, preco: float):
         self.codigo = codigo
         self.descricao = descricao
         self.preco = preco
 class Venda:
-    def __init__(self, quantidade: int, dataVenda: date):
+    def __init__(self, quantidade: int, dataVenda: date, fornecedor, produto):
         self.quantidade = quantidade
         self.datavenda = dataVenda
-        
+
