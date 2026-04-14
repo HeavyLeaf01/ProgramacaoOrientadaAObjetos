a6e198ca37a8844f57b15df0d4a712960ded9464
HeavyLeaf <gabigoolgabriell@gmail.com>
2026-04-14 15:26:12 -0300

    Update Atividade13.py

--- Atividade13.py
+++ Atividade13.py
@@ -1,22 +1,52 @@
 from datetime import date
+
+class Venda:
+    def __init__(self, quantidade: int, dataVenda: date, fornecedor, produto):
+        self.quantidade = quantidade
+        self.datavenda = dataVenda
+        self.fornecedor = fornecedor
+        self.produto = produto
+class Produto:
+    def __init__(self, codigo: int, descricao: str, preco: float):
+        self.__codigo = codigo
+        self.descricao = descricao
+        self.__preco = preco
+    @property
+    def codigo(self):
+        return self.__codigo
+    @property
+    def preco(self):
+        return self.__preco
+    @preco.setter
+    def preco(self,preco):
+        if preco > self.preco:
+            self.preco = preco
 class Fornecedor:
     def __init__(self, CNPJ: str,nome: str):
         self.CNPJ = CNPJ
         self.nome = nome
         self.listaVendaProdutos = []
 
-    def addVenda(self, q, dv, p):
-        self.listaVendaProdutos.append(Venda(self,q,dv,p))
+    def addVenda(self, q, datavenda, produto):
+        objeto = Venda(self, q, datavenda, produto)
+        self.listaVendaProdutos.append(objeto)
     def emitirNota(self, di: date, df: date) -> float:
-        pass
+        soma = 0.0
+        for item in self.listaVendaProdutos:
+            if di <= item.dataVenda <= df:
+                total += (item.quantidade * item.produto.preco)
+        return total
 
-class Produto:
-    def __init__(self, codigo: int, descricao: str, preco: float):
-        self.codigo = codigo
-        self.descricao = descricao
-        self.preco = preco
-class Venda:
-    def __init__(self, quantidade: int, dataVenda: date, fornecedor, produto):
-        self.quantidade = quantidade
-        self.datavenda = dataVenda
+identificacao = input("Informe o número do CNPJ da empresa: ")
+razaoSocial = input("Informe o nome do fornecedor: ")
+empresa = Fornecedor(identificacao, razaoSocial)
 
+quantas = int(input("Quanta vendas realizadas?: "))
+for i in range(0,quantas):
+    codProd = int(input("Código do produto: "))
+    marca = input("Descrição da marca do produto: ")
+    preco = float(input("Quanto custa? R$:  "))
+    quant = int(input("Quantidade vendida desse produto: "))
+    dataV = date(int(input("Ano: ")), int(input("Mês: ")), int(input("Dia: ")))
+    empresa.addVenda(quant, dataV, Produto(codProd,marca,preco))
+    print(f"Venda cadastrada com sucesso em {dataV.strftime("%d/%m/%Y")}")
