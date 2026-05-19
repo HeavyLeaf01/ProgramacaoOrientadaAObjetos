d66b6e652bc52138711260fa6c968ca7b115c4fb
HeavyLeaf <gabigoolgabriell@gmail.com>
2026-05-19 15:29:46 -0300

    Update

--- 2Trimestre/FIFA2026.py
+++ 2Trimestre/FIFA2026.py
@@ -1,6 +1,37 @@
 from Atividade1 import *
 import os
 
+def cadastrarTimes(listaTimes):
+    nome = input("Informe o nome do time: ")
+    titulos = int(input("Informe a quantidade de títulos mundiais: "))
+    listaTimes += [TIme(nome, titulos)]
+    print(f"Time {nome} cadastrado com sucesso! =)")
+
+def cadastrarJogador(listaTimes):
+    if len(listaTimes) == 0:
+        print("Impossível cadastrar jogador: nenhum time cadastrado! =()")
+    else:
+        nome = input("Informe o nome do jogador: ")
+        idade = int(input("Informe a idade do jogador: "))
+        escalado = False
+        for time in listaTImes:
+            resposta = input(f"Incluir {nome} no time {time.nome}? (s/n) ")
+            if resposta == "s":
+                time.addJogador(nome, idade)
+                escalado = True
+                print(f"Jogador {nome} cadastrado com sucesso! =)")
+                break
+        if not escalando:
+            print("Impossível cadastrar jogador: não foi escalado para nenhum time! =(")
+
+def exibirTimescadastrados(listaTimes):
+    if len(listaTimes) == 0:
+        print("Nenhum time cadastrado ainda! =/")
+    else:
+        for time in listaTimes:
+            print(f"{tme.nome} ({time.titulos} copas do mundo)")
+
+listaTimes = []
 while True:
     os.system("cls")
     print("***  COPA DO MUNDO FIFA 2026  ***\n")
@@ -8,13 +39,14 @@ while True:
     print("* Sistema Internacional de Fãs *\n")
     print("   (01) Cadastrar times")
     print("   (02) Cadastrar Jogadores")
+    print("   (03) Exibir times cadastrados")
     print("   (10) Sair")
 
     opcao = int(input("    Informe a opção desejando: "))
     if opcao == 1:
-        pass
+        cadastrarTimes(listaTimes)
     elif opcao == 2:
-        pass
+        cadastrarJogador(listaTimes)
     elif opcao == 10:
         break
     else:
